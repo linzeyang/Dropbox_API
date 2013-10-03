0159d9a57107de404066f1c96db177b4b17907e2
linzeyang <zeyanglin2013@gmail.com>
2013-10-03 13:49:56 +0100

    first actual commit

---
+++ dropbox_api/views.py
@@ -0,0 +1,11 @@
+# Create your views here.
+from django.shortcuts import render_to_response
+
+def start(request):
+    return render_to_response('start.html')
+	
+def dropbox_dropin(request):
+	return render_to_response('dropbox-dropin.html')
+
+def dropbox_datastore(request):
+	return render_to_response('dropbox-datastore.html')
