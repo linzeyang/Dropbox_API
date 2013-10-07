ff31ba38439366198a359407dc5e1d3d2bc2e132
linzeyang <zeyanglin2013@gmail.com>
2013-10-07 14:40:12 +0100

    optimize html structure and js code

--- static/dropbox-dropin-chooser.js
+++ static/dropbox-dropin-chooser.js
@@ -1,7 +1,5 @@
 function onReady() {
 
-    console.log("Dropbox Drop-ins Chooser Ready!");
-
     if( Dropbox.isBrowserSupported() )
     {
         console.log( "Dropbox chooser is supported by your browser !" );
@@ -12,22 +10,23 @@ function onReady() {
     }
 
     //*********** Dropbox's own chooser button ***************
+
     var buttonDbChooser = document.getElementById( "db-chooser" );
     
-    $( "#slt1" ).change( function(e) {
-        $( buttonDbChooser ).attr( "data-multiselect", $( "#slt1" ).val() );
+    $( "#select-db-chooser" ).change( function(e) {
+        $( buttonDbChooser ).attr( "data-multiselect", $( "#select-db-chooser" ).val() );
     } );
 
-    var $table1 = $( "#tbl1" );
+    var $tableDbChooser = $( "#table-db-chooser" );
 
     buttonDbChooser.addEventListener( "DbxChooserSuccess", function(e) {
-        $table1.find( ".fileline" ).remove();
+        $tableDbChooser.find( ".fileline" ).remove();
 
         e.files.forEach( function( elem ) {
             var name = elem["name"];
             var link = "<a target=\"_blank\" href=\"" + elem["link"] + "\">Click</a>";
             var bytes = elem["bytes"];
-            var icon = "<img src=\"" + elem["icon"] + "\" />";
+            var icon = "<img src=\"" + elem["icon"] + "\" alt=\"" + name + "\" />";
             var thumbnail = "N/A";
 
             if( elem["thumbnails"]["64x64"] != undefined )
@@ -35,7 +34,7 @@ function onReady() {
                 thumbnail = "<img alt=\"thumbnail\" src=\"" + elem["thumbnails"]["64x64"] + "\" />";
             }
 
-            $table1.append( 
+            $tableDbChooser.append( 
                 "<tr class=\"fileline\"><td>" + name + "</td>" + 
                 "<td>" + link + "</td>" + 
                 "<td>" + bytes + "</td>" + 
@@ -43,48 +42,45 @@ function onReady() {
                 "<td>" + thumbnail + "</td></tr>" 
             );
         });
-
-        $( ".filepath" ).val( e.files[0].link );
-
     }, false);
 
     buttonDbChooser.addEventListener( "DbxChooserCancel", function(e) {
-        console.log( "Canceled" );
+        console.log( "Canceled !" );
     }, false);
+
     //********************************************************
 
     //*********** HTML standard button ***********************
+
     var $buttonChooser = $( "#db-chooser-button" );
     
-    var $table2 = $( "#tbl2" );
+    var $tableStdChooser = $( "#table-std-chooser" );
 
     var chooserOptions = {
         linkType: "preview",
         multiselect: true,
         success: function( files ) {
-            $table2.find( ".fileline" ).remove();
+            $tableStdChooser.find( ".fileline" ).remove();
 
             files.forEach( function( elem ) {
                 var name = elem["name"];
                 var link = "<a target=\"_blank\" href=\"" + elem["link"] + "\">Click</a>";
                 var bytes = elem["bytes"];
             
-                $table2.append( 
+                $tableStdChooser.append( 
                     "<tr class=\"fileline\"><td>" + name + "</td>" + 
                     "<td>" + link + "</td>" + 
                     "<td>" + bytes + "</td></tr>" 
                 );
             } );
-
-            $( ".filepath" ).val( files[0]["link"] );
         },
         cancel: function() {
-            console.log( "Canceled" );
+            console.log( "Canceled !" );
         }
     };
 
-    $( "input[type='radio']" ).change( function(e) {
-        chooserOptions["linkType"] = $( "input[type='radio']:checked" ).val();
+    $( "#div-std-chooser input[type='radio']" ).change( function(e) {
+        chooserOptions["linkType"] = $( "#div-std-chooser input[type='radio']:checked" ).val();
     });
 
     $buttonChooser.click( function( event ) {
