ad1bd85d0ee88ffe336e06db8969bf30df156e24
linzeyang <zeyanglin2013@gmail.com>
2013-10-15 11:06:55 +0100

    use render() instead of render_to_response()

--- dropbox_api/views.py
+++ dropbox_api/views.py
@@ -1,14 +1,14 @@
 # Create your views here.
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 
 def start(request):
-    return render_to_response('start.html')
+    return render(request, 'start.html')
 	
 def dropbox_dropin_chooser(request):
-	return render_to_response('dropbox-dropin-chooser.html')
+	return render(request, 'dropbox-dropin-chooser.html')
 
 def dropbox_dropin_saver(request):
-	return render_to_response('dropbox-dropin-saver.html')
+	return render(request, 'dropbox-dropin-saver.html')
 
 def dropbox_datastore(request):
-	return render_to_response('dropbox-datastore.html')
+	return render(request, 'dropbox-datastore.html')
