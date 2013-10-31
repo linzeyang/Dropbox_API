d7ae7ba99b472cb1a97ab66f9bfe2f4c570a988f
linzeyang <zeyanglin2013@gmail.com>
2013-10-31 19:29:15 +0000

    Apply templates and static files app-specific

--- dropbox_api/settings.py
+++ dropbox_api/settings.py
@@ -61,7 +61,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'D:/DjangoProjects/Dropbox_API/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -72,7 +72,6 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "D:/DjangoProjects/dropbox_api/static",
 )
 
 # List of finder classes that know how to find static files in
@@ -112,7 +111,6 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "D:/DjangoProjects/dropbox_api/templates",
 )
 
 INSTALLED_APPS = (
@@ -122,6 +120,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dropbox_api',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
--- dropbox_api/views.py
+++ dropbox_api/views.py
@@ -2,13 +2,13 @@
 from django.shortcuts import render
 
 def start(request):
-    return render(request, 'start.html')
+    return render(request, 'dropbox_api/start.html')
 	
 def dropbox_dropin_chooser(request):
-	return render(request, 'dropbox-dropin-chooser.html')
+	return render(request, 'dropbox_api/dropbox-dropin-chooser.html')
 
 def dropbox_dropin_saver(request):
-	return render(request, 'dropbox-dropin-saver.html')
+	return render(request, 'dropbox_api/dropbox-dropin-saver.html')
 
 def dropbox_datastore(request):
-	return render(request, 'dropbox-datastore.html')
+	return render(request, 'dropbox_api/dropbox-datastore.html')
