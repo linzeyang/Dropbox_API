44af955123b0163c343a398d0fc1c886b9758840
linzeyang <zeyanglin2013@gmail.com>
2014-01-31 10:11:36 +0000

    Upgrade to Bootstrap 3.1.0

--- dropbox_api/views.py
+++ dropbox_api/views.py
@@ -1,6 +1,11 @@
 # Create your views here.
 from django.shortcuts import render
 
+from dropbox.client import DropboxClient, DropboxOAuth2Flow, DropboxOAuth2FlowNoRedirect
+from dropbox.rest import ErrorResponse, RESTSocketError
+from dropbox.datastore import DatastoreError, DatastoreManager, Date, Bytes
+
+
 def start(request):
     return render(request, 'dropbox_api/start.html')
 	
@@ -14,4 +19,15 @@ def datastore_js(request):
 	return render(request, 'dropbox_api/datastore-js.html')
 
 def datastore_python(request):
-    return render(request, 'dropbox_api/datastore-python.html')
+    context = ds_py_client()
+    return render(request, 'dropbox_api/datastore-python.html', context)
+
+def ds_py_client():
+    # APP_KEY = 'hun9jgu2dpb3374'
+    # APP_SECRET = 'zw1ntdvbn8bd73d'
+
+    # redirect_uri = '/datastore-python/'
+
+    # DropboxOAuth2Flow(APP_KEY, APP_SECRET, )
+
+    return {'status': 'to be completed'}
